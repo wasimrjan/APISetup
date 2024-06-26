27442df4aeee4a0563759d901a47534d1fac30fe
wasimrjan <wasim.rjan@gmail.com>
2024-06-26 22:33:42 +0530

    done

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,23 @@
+var mysql = require('mysql');
+
 exports.ProudctShow = (req,res)=>{
-    var id = req.params.id;
-    res.send("Product Details : " + id);
+  //  var id = req.params.id;
+    //res.send("Product Details : " + id);
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM t_data", function (err, result, fields) {
+    if (err) throw err;
+     res.send(result);
+  });
+});
+
 }
 
 exports.ProudctUpdate = (req,res)=>{
--- exp_cnt.js
+++ exp_cnt.js
@@ -2,6 +2,8 @@ const express = require('express');
 const api = express();
 const productRouter = require('./routers/product');
 
+
+
 api.use('/product',productRouter.route);
 
 api.listen(8080);
